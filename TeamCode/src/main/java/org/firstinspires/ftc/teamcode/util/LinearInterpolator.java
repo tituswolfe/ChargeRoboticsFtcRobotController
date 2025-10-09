9150772b3ecb386738638c8a751b006ddd858cc1
Jonny <jonnyking1024@icloud.com>
2025-10-08 19:24:45 -0500

    I added two arrays to the LinearInterpolator class to contain data on field position and launch angles. I also created a function that linearly interpolates between data points and returns an angle based on a position.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/LinearInterpolator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/LinearInterpolator.java
@@ -1,5 +1,33 @@
 package org.firstinspires.ftc.teamcode.util;
 
 public class LinearInterpolator {
+    //These arrays contain data on various distances and the required angle of launch.
+    private static Double[] dataPosition = new Double[]{
+           1.,2.,3.
+    };
+    private static Double[] dataAngle = new Double[]{
+         2.,4.,6.
+    };
 
+    //This function returns angle of launch based on robot distance from goal.
+    public static Double getAngle(Double distance){
+        Double angle;
+        int secondIndex = 0;
+
+        for (int i = 0; i < dataPosition.length; i++){
+            if (distance < dataPosition[i]){
+                secondIndex = i;
+                break;
+            }
+        }
+
+        if (secondIndex == 0){
+            return null;
+        }
+
+        angle = dataAngle[secondIndex - 1] + (dataAngle[secondIndex] - dataAngle[secondIndex - 1]) /
+                (dataPosition[secondIndex] - dataPosition[secondIndex - 1]) *
+                (distance - dataPosition[secondIndex - 1]);
+        return angle;
+    }
 }
