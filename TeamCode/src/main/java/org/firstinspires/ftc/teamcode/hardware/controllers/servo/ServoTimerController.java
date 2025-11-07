d562d0132a97702148bc7935ee99e68186004d21
PAGMA Online <119758931+PAGMAYouTube@users.noreply.github.com>
2025-11-07 13:42:52 -0600

    Move path building to op modes. Restructure to remove sleeps.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/controllers/servo/ServoTimerController.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.hardware.controllers.servo;
+
+import com.pedropathing.util.Timer;
+
+public class ServoTimerController {
+    private final Timer actionTimer;
+    // private boolean isA
+
+    public ServoTimerController() {
+        this.actionTimer = new Timer();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/jetfire/JetFireGamepadMapping2.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.robots.jetfire;
+
+public class JetFireGamepadMapping2 extends JetFireGamepadMapping {
+    public JetFireGamepadMapping2(JetFireRobot decodeRobot) {
+        super(decodeRobot);
+    }
+
+    @Override
+    public void onLeftStickPressed() {
+        super.onRightStickPressed();
+    }
+
+    @Override
+    public void onRightStickPressed() {
+        super.onLeftStickPressed();
+    }
+}
