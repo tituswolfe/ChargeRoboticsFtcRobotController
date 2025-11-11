212519d5b76d05e8ef0cdabb77c9b5a0afa528a2
PAGMA <emailpamga@gmail.com>
2025-11-11 11:14:57 -0600

    Update LinearInterpolator

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/LinearInterpolator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/LinearInterpolator.java
@@ -1,39 +1,55 @@
 package org.firstinspires.ftc.teamcode.util;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * {@link LinearInterpolator} Has a function to interpolate data and estimate launch angle.
+ *
  * @author Jonny King
+ * @author Titus
  */
 
 public class LinearInterpolator {
-    //These arrays contain data on various distances and the required angle of launch.
-    private static Double[] dataPosition;
-    private static Double[] dataAngle;
+    private final TreeMap<Double, Double> dataPoints;
 
-    public LinearInterpolator(Double[] positions, Double[] angles){
-        dataPosition = positions;
-        dataAngle = angles;
+    public LinearInterpolator(TreeMap<Double, Double> dataPoints){
+        this.dataPoints = dataPoints;
     }
 
-    //This function returns angle of launch based on robot distance from goal.
-    public static Double getAngle(Double distance){
-        Double angle;
-        int secondIndex = 0;
+    /**
+     * Performs linear interpolation to estimate the y-value for a given x.
+     * @param x The x-value to interpolate for.
+     * @return The interpolated y-value.
+     */
+    public double interpolate(double x){
+        if (dataPoints.isEmpty()) return 0;
+        if (dataPoints.containsKey(x)) return dataPoints.get(x);
 
-        for (int i = 0; i < dataPosition.length; i++){
-            if (distance < dataPosition[i]){
-                secondIndex = i;
-                break;
-            }
-        }
+        Map.Entry<Double, Double> floorEntry = dataPoints.floorEntry(x);
+        Map.Entry<Double, Double> ceilingEntry = dataPoints.ceilingEntry(x);
 
-        if (secondIndex == 0){
-            return null;
-        }
+        if (floorEntry == null) return ceilingEntry.getValue();
+        if (ceilingEntry == null) return floorEntry.getValue();
 
-        angle = dataAngle[secondIndex - 1] + (dataAngle[secondIndex] - dataAngle[secondIndex - 1]) /
-                (dataPosition[secondIndex] - dataPosition[secondIndex - 1]) *
-                (distance - dataPosition[secondIndex - 1]);
-        return angle;
+        double x1 = floorEntry.getKey();
+        double y1 = floorEntry.getValue();
+        double x2 = ceilingEntry.getKey();
+        double y2 = ceilingEntry.getValue();
+
+        double t = (x - x1) / (x2 - x1);
+
+        return lerp(y1, y2, t);
+    }
+
+    /**
+     * Calculates the linear interpolation (lerp) between two values.
+     * @param v0 The starting value (y1).
+     * @param v1 The ending value (y2).
+     * @param t The interpolation factor, typically between 0 and 1.
+     * @return The interpolated value.
+     */
+    private static double lerp(double v0, double v1, double t) {
+        return v0 + t * (v1 - v0);
     }
 }
