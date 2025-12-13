1fbc26d6ecb271bb2b50aa2236feb5b083b1b1f3
PAGMA <emailpamga@gmail.com>
2025-12-13 13:42:33 -0600

    Various.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/controllers/motor/AngleMotorController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/controllers/motor/AngleMotorController.java
@@ -12,7 +12,7 @@ import org.firstinspires.ftc.teamcode.util.math.Angle;
 public class AngleMotorController extends MotorController {
     public final Angle maxPositiveLimit;
     public final Angle minNegativeLimit;
-    PIDFController pidfController = new PIDFController(new PIDFCoefficients(0.012, 0, 0, 0));
+    PIDFController pidfController = new PIDFController(new PIDFCoefficients(0.015, 0, 0, 0));
     private Angle targetHeading = new Angle(0);
 
     public AngleMotorController(DcMotorEx dcMotorEx, double ticksPerRevolution, double totalGearRatio, Angle maxPositiveLimit, Angle minNegativeLimit) {
@@ -28,9 +28,14 @@ public class AngleMotorController extends MotorController {
     }
 
     public void setTargetHeading(Angle angle) {
-        targetHeading = angle;
 
-//        if (targetHeading > maxPositiveLimit.getAngle(Angle.AngleUnit.DEGREES))
+        if (angle.getAngle(Angle.AngleSystem.SIGNED) > maxPositiveLimit.getAngle(Angle.AngleSystem.SIGNED)) {
+            targetHeading = maxPositiveLimit;
+        } else if (angle.getAngle(Angle.AngleSystem.SIGNED) < minNegativeLimit.getAngle(Angle.AngleSystem.SIGNED)) {
+            targetHeading = minNegativeLimit;
+        } else {
+            targetHeading = angle;
+        }
 //        targetHeading = new Angle(Math.min(
 //                Math.max(
 //                        angle.getAngle(Angle.AngleUnit.RADIANS, Angle.AngleSystem.SIGNED),
